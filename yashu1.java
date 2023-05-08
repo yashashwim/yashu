3c83045ea81549dde5806802590e61dc68aa1552
yashashwim <132894895+yashashwim@users.noreply.github.com>
2023-05-08 03:13:31 -0700

    Update yashu1.java

--- yashu1.java
+++ yashu1.java
@@ -5,5 +5,6 @@ public static void main (String args[])
 System.out.println("hello");
 System.out.println(" AM FINE");
 System.out.println("Where Are you");
+System.out.println("am at mysore");
 }
 }
